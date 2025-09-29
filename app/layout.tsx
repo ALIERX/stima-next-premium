97ca43c488be5601cf2f68e26e3ef46f98a8e7b1
Alex Khamraev <a.khamraev@gmail.com>
2025-09-30 01:19:01 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import './globals.css'
+import '../styles/globals.css'
 import type { Metadata } from 'next'
 import SoundTapticProvider from '@/components/SoundTapticProvider'
 import Link from 'next/link'
